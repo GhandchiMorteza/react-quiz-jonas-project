c7d7158af0398744d15949fb9406df3591bfdeb2
GhandchiMorteza <mortezaghandchi7@gmail.com>
2024-02-14 22:36:14 +0330

    handling the questions

---
+++ src/components/App.js
@@ -0,0 +1,73 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'start':
+      return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    default:
+      throw new Error('Invalid action was dispatched! ' + action.type);
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = state.questions.length;
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {state.status === 'loading' && <Loader />}
+        {state.status === 'error' && <Error />}
+        {state.status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {state.status === 'active' && (
+          <Question
+            question={state.questions[state.index]}
+            dispatch={dispatch}
+            answer={state.answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
